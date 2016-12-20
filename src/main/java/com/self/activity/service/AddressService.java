7ef106f2148c0d7da67a126ee1434e1392fe4323
dm86xq4a <1035902059@qq.com>
2016-12-20 11:32:58 +0800

    调整domain返回app的方式

--- src/main/java/com/self/activity/service/AddressService.java
+++ src/main/java/com/self/activity/service/AddressService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,7 +14,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.self.activity.common.Constants;
+import com.self.activity.common.Convert;
 import com.self.activity.sdk.bean.Result;
 import com.self.activity.vo.ProvinceForSolr;
 
@@ -24,25 +28,35 @@ public class AddressService {
 	
 	public Result<List<ProvinceForSolr>> searchPro(String context,Pageable pageable,HttpHeaders headers){
 		HttpEntity<?> requestEntity = new HttpEntity<>(null,headers);
-		ResponseEntity<Result> response;
+		ResponseEntity<Result<List<ProvinceForSolr>>> response;
 		try {
 			response = client.exchange(
 					Constants.PROVINCE+"?context="+context+"&page="+pageable.getPageNumber()+"&size="+pageable.getPageSize(), HttpMethod.GET, requestEntity,
-					Result.class);
+					new ParameterizedTypeReference<Result<List<ProvinceForSolr>>>() {
+					});
 		} catch (RestClientException e) {
 			return new Result("10003", e);
 		}
-		return response.getBody();
+		//Result body = response.getBody();
+		//System.out.println(JSON.toJSONString(body));
+		Result<List<ProvinceForSolr>> obj =response.getBody();
+		List<ProvinceForSolr> data = obj.getData();
+		System.out.println(data);
+		for (ProvinceForSolr provinceForSolr : data) {
+			System.out.println(provinceForSolr.getProvinceId());
+		}
+		return obj;
 	}
 	public Result<ProvinceForSolr> save(ProvinceForSolr provinceForSolr,HttpHeaders headers){
 		HttpEntity<?> requestEntity = new HttpEntity<>(provinceForSolr,headers);
-		ResponseEntity<Result> response;
+		ResponseEntity<Result<ProvinceForSolr>> response;
 		try {
 			response = client.exchange(
 					Constants.PROVINCE, HttpMethod.POST, requestEntity,
-					Result.class);
+					new ParameterizedTypeReference<Result<ProvinceForSolr>>() {
+					});
 		} catch (RestClientException e) {
-			return new Result("10003", e);
+			return new Result<ProvinceForSolr>("10003", e);
 		}
 		return response.getBody();
 	}
